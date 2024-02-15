c417a7645cd600ed7c577d7f0d88025f9ce499d3
Logan Sailer <logansailer0633@gmail.com>
2024-02-15 15:58:50 +0900

    basic page setup and linking

---
+++ scripts.js
@@ -0,0 +1,24 @@
+//factory to create board IIFE
+//factory to keep score using play 1 name and pkayer 2 name
+//randomizer to see who goes first
+//How it should work: CP picks random empty space, player picks empty space.
+//when x or o placed, index into array and place x or o
+
+//things to consider: cant pick taken space, how to evaluate win conditions
+
+const gameBoard = (function () {
+    const board = ['','','','','','','','','']
+    return { board };
+})();
+
+function player();
+
+function gameController() {
+    let playerOne = "Player One";
+    let cpu = "Computer Player";
+
+    
+};
+
+console.log(gameBoard.board)
+
